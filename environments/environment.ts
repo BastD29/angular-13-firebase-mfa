e1e27e9be08f73de1122e4f7b9717ed9105636de
Bastien Daniel <bastien.daniel@rawfish.com>
2023-05-08 12:12:08 +0200

    first commit

---
+++ environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDddYd9bBXDav8D_PhgQWBUndUsCDAQkVY',
+    authDomain: 'react-auth-dev-bbb7d.firebaseapp.com',
+    databaseURL:
+      'https://react-auth-dev-bbb7d-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'react-auth-dev-bbb7d',
+    storageBucket: 'react-auth-dev-bbb7d.appspot.com',
+    messagingSenderId: '3040780869',
+    appId: '1:3040780869:web:c4a652347a21ca28e5792c',
+  },
+};
